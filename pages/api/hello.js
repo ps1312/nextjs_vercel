f3d5d3ff6071b54fc1ddae209bf727ab608283ae
Paulo Sérgio <ps131296@gmail.com>
2021-03-24 18:08:18 -0300

    add request to test in vercel

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default async (req, res) => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data = await response.json();
+
+  res.status(200).json(data);
+};
