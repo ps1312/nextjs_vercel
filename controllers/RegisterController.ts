f2fa3b4d134000b34a92099f289155c704a01a12
Paulo Sérgio <ps131296@gmail.com>
2021-03-25 11:08:09 -0300

    RegisterController should return BAD REQUEST status and a message on missing passwordConfirmation

--- controllers/RegisterController.ts
+++ controllers/RegisterController.ts
@@ -4,12 +4,19 @@ class RegisterController {
       return { statusCode: 400, body: "Missing parameters: Email" };
     }
 
-    return { statusCode: 400, body: "Missing parameters: Password" };
+    if (!body.password) {
+      return { statusCode: 400, body: "Missing parameters: Password" };
+    }
+
+    return {
+      statusCode: 400,
+      body: "Missing parameters: Password Confirmation",
+    };
   }
 }
 
 module RegisterController {
-  export type Result = { statusCode: number; body: string }
+  export type Result = { statusCode: number; body: string };
 }
 
-export default RegisterController
+export default RegisterController;
--- tests/RegisterController.spec.ts
+++ tests/RegisterController.spec.ts
@@ -13,10 +13,19 @@ describe("RegisterController.ts", () => {
   it("should return BAD REQUEST status and a message on missing password", () => {
     const sut = new RegisterController();
 
-    const result = sut.process({ email: 'any-email@mail.com ' });
+    const result = sut.process({ email: 'any-email@mail.com' });
 
     expect(result.statusCode).toEqual(400);
     expect(result.body).toEqual("Missing parameters: Password");
+  });
+
+  it("should return BAD REQUEST status and a message on missing passwordConfirmation", () => {
+    const sut = new RegisterController();
+
+    const result = sut.process({ email: 'any-email@mail.com', password: 'any password' });
+
+    expect(result.statusCode).toEqual(400);
+    expect(result.body).toEqual("Missing parameters: Password Confirmation");
   });
 });
 
