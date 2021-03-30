0e0bb66de2e82c3d33b49f4ee4107c17b796d0af
Paulo Sérgio <ps131296@gmail.com>
2021-03-29 22:09:39 -0300

    refactored MissingParamsValidator to return a result instead of throwing

--- server/validators/MissingParamsValidator.ts
+++ server/validators/MissingParamsValidator.ts
@@ -1,14 +1,14 @@
 import MissingParamError from "../errors/MissingParamsError";
 
 export default class MissingParamsValidator {
-  static validate(body: any, requiredParams: string[]) {
+  static validate(body: any, requiredParams: string[]): Error | undefined {
     let missingParams: string[] = [];
 
     const addOnPresentParam = (p: string) => !body[p] && missingParams.push(p)
     requiredParams.forEach(addOnPresentParam)
 
     if (missingParams.length > 0) {
-      throw new MissingParamError(missingParams.join(', '))
+      return new MissingParamError(missingParams.join(', '))
     }
   }
 }
--- tests/server/MissingParamsValidator.spec.ts
+++ tests/server/MissingParamsValidator.spec.ts
@@ -5,23 +5,18 @@ describe('MissingParamsValidator.ts', () => {
   it('should throw if body does not have all required params', () => {
     const requiredParams1 = ['param1', 'param2']
     const body1 = { param1: "value" }
-
-    expect(() => {
-      MissingParamsValidator.validate(body1, requiredParams1)
-    }).toThrow(new MissingParamError('param2'))
+    const result1 = MissingParamsValidator.validate(body1, requiredParams1)
+    expect(result1).toEqual(new MissingParamError('param2'))
 
     const requiredParams2 = ['param1', 'param2']
     const body2 = {}
-    expect(() => {
-      MissingParamsValidator.validate(body2, requiredParams2)
-    }).toThrow(new MissingParamError('param1, param2'))
+    const result2 = MissingParamsValidator.validate(body2, requiredParams2)
+    expect(result2).toEqual(new MissingParamError('param1, param2'))
 
     const requiredParams3 = ['param1', 'param2']
     const body3 = { param1: 'value', param2: 'value' }
-    expect(() => {
-      MissingParamsValidator.validate(body3, requiredParams3)
-    }).not.toThrow()
-
+    const result3 = MissingParamsValidator.validate(body3, requiredParams3)
+    expect(result3).toEqual(undefined)
   })
 })
 
